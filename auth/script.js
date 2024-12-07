a57d154c21b50e38e8df036d64fa74862226a408
Jaiprakash-JH <mail.jaiprakash.jh@gmail.com>
2024-12-07 21:00:41 +0530

    JH Updated

---
+++ auth/script.js
@@ -0,0 +1,22 @@
+const loginBtn = document.getElementById('loginBtn');
+const signupBtn = document.getElementById('signupBtn');
+const loginForm = document.getElementById('loginForm');
+const signupForm = document.getElementById('signupForm');
+
+loginBtn.addEventListener('click', () => {
+  loginForm.classList.add('active');
+  signupForm.classList.remove('active');
+  loginBtn.classList.add('active');
+  signupBtn.classList.remove('active');
+});
+
+signupBtn.addEventListener('click', () => {
+  signupForm.classList.add('active');
+  loginForm.classList.remove('active');
+  signupBtn.classList.add('active');
+  loginBtn.classList.remove('active');
+});
+// Disable right-click functionality
+document.addEventListener('contextmenu', function (e) {
+    e.preventDefault(); // Prevent the context menu from appearing
+  });
